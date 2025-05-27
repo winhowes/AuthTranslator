7d9e6c30e89a389cfa3f91288b72f40c4def4f10
Winston Howes <winstonhowes@gmail.com>
2025-05-27 11:34:12 -0700

    log upstream status codes

---
+++ app/statusrecorder.go
@@ -0,0 +1,21 @@
+package main
+
+import "net/http"
+
+// statusRecorder captures the HTTP status code written by a handler.
+type statusRecorder struct {
+	http.ResponseWriter
+	status int
+}
+
+func (r *statusRecorder) WriteHeader(code int) {
+	r.status = code
+	r.ResponseWriter.WriteHeader(code)
+}
+
+func (r *statusRecorder) Write(b []byte) (int, error) {
+	if r.status == 0 {
+		r.status = http.StatusOK
+	}
+	return r.ResponseWriter.Write(b)
+}
