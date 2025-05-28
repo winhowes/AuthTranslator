891b56104e3a78ed8c23faf56927b1113f0eee11
Winston Howes <winstonhowes@gmail.com>
2025-05-27 20:32:07 -0700

    Add plugin aggregator packages

---
+++ app/integrationplugins/plugins/plugins.go
@@ -0,0 +1,20 @@
+package plugins
+
+import (
+	_ "github.com/winhowes/AuthTranslator/app/integrationplugins/asana"
+	_ "github.com/winhowes/AuthTranslator/app/integrationplugins/confluence"
+	_ "github.com/winhowes/AuthTranslator/app/integrationplugins/ghe"
+	_ "github.com/winhowes/AuthTranslator/app/integrationplugins/github"
+	_ "github.com/winhowes/AuthTranslator/app/integrationplugins/gitlab"
+	_ "github.com/winhowes/AuthTranslator/app/integrationplugins/jira"
+	_ "github.com/winhowes/AuthTranslator/app/integrationplugins/linear"
+	_ "github.com/winhowes/AuthTranslator/app/integrationplugins/monday"
+	_ "github.com/winhowes/AuthTranslator/app/integrationplugins/okta"
+	_ "github.com/winhowes/AuthTranslator/app/integrationplugins/sendgrid"
+	_ "github.com/winhowes/AuthTranslator/app/integrationplugins/servicenow"
+	_ "github.com/winhowes/AuthTranslator/app/integrationplugins/slack"
+	_ "github.com/winhowes/AuthTranslator/app/integrationplugins/stripe"
+	_ "github.com/winhowes/AuthTranslator/app/integrationplugins/trufflehog"
+	_ "github.com/winhowes/AuthTranslator/app/integrationplugins/twilio"
+	_ "github.com/winhowes/AuthTranslator/app/integrationplugins/zendesk"
+)
