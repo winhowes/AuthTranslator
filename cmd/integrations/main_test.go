d95dad0b61e696a19ea1cf478518cd4d005b6be1
Winston Howes <winstonhowes@gmail.com>
2025-05-29 11:52:01 -0700

    Fix go vet errors

--- cmd/integrations/main_test.go
+++ cmd/integrations/main_test.go
@@ -1,16 +1,20 @@
 package main
 
 import (
-	"flag"
-	"io"
-	"os"
-	"os/exec"
-  	"path/filepath"
-	"strings"
-	"testing"
+        "flag"
+        "io"
+        "os"
+        "os/exec"
+        "path/filepath"
+        "strings"
+        "testing"
+        "net/http"
+        "net/http/httptest"
 
-	"github.com/winhowes/AuthTranslator/cmd/integrations/plugins"
+        "github.com/winhowes/AuthTranslator/cmd/integrations/plugins"
 )
+
+var server = flag.String("server", "http://localhost:8080/integrations", "integration endpoint")
 
 func TestAddUpdateDeleteList(t *testing.T) {
 	dir := t.TempDir()
