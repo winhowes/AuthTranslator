ca1d5f327c4ce8b8b027d879532a0bc94d905a8a
Winston Howes <winstonhowes@gmail.com>
2025-05-28 23:30:15 -0700

    Fix allowlist tests by handling empty params

--- cmd/allowlist/plugins/types.go
+++ cmd/allowlist/plugins/types.go
@@ -2,27 +2,27 @@ package plugins
 
 // CallerConfig mirrors the server structure for CLI use.
 type CallerConfig struct {
-	ID           string             `json:"id"`
-	Capabilities []CapabilityConfig `json:"capabilities,omitempty"`
-	Rules        []CallRule         `json:"rules"`
+	ID           string             `json:"id" yaml:"id"`
+	Capabilities []CapabilityConfig `json:"capabilities,omitempty" yaml:"capabilities,omitempty"`
+	Rules        []CallRule         `json:"rules,omitempty" yaml:"rules,omitempty"`
 }
 
 type CapabilityConfig struct {
-	Name   string                 `json:"name"`
-	Params map[string]interface{} `json:"params"`
+	Name   string                 `json:"name" yaml:"name"`
+	Params map[string]interface{} `json:"params" yaml:"params"`
 }
 
 type CallRule struct {
-	Path    string                       `json:"path"`
-	Methods map[string]RequestConstraint `json:"methods"`
+	Path    string                       `json:"path" yaml:"path"`
+	Methods map[string]RequestConstraint `json:"methods,omitempty" yaml:"methods,omitempty"`
 }
 
 type RequestConstraint struct {
-	Headers []string               `json:"headers"`
-	Body    map[string]interface{} `json:"body"`
+	Headers []string               `json:"headers,omitempty" yaml:"headers,omitempty"`
+	Body    map[string]interface{} `json:"body,omitempty" yaml:"body,omitempty"`
 }
 
 type AllowlistEntry struct {
-	Integration string         `json:"integration"`
-	Callers     []CallerConfig `json:"callers"`
+	Integration string         `json:"integration" yaml:"integration"`
+	Callers     []CallerConfig `json:"callers,omitempty" yaml:"callers,omitempty"`
 }
