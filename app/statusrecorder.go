5572561f65ce7d3028ad382a6b160e912303f413
Winston Howes <winstonhowes@gmail.com>
2025-05-29 21:53:15 -0700

    Add gRPC and WebSocket support

--- app/statusrecorder.go
+++ app/statusrecorder.go
@@ -1,11 +1,30 @@
 package main
 
-import "net/http"
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"net/http"
+)
 
 // statusRecorder captures the HTTP status code written by a handler.
 type statusRecorder struct {
 	http.ResponseWriter
 	status int
+}
+
+// ensure interfaces are forwarded when supported by the underlying writer
+func (r *statusRecorder) Hijack() (net.Conn, *bufio.ReadWriter, error) {
+	if h, ok := r.ResponseWriter.(http.Hijacker); ok {
+		return h.Hijack()
+	}
+	return nil, nil, fmt.Errorf("hijacker not supported")
+}
+
+func (r *statusRecorder) Flush() {
+	if f, ok := r.ResponseWriter.(http.Flusher); ok {
+		f.Flush()
+	}
 }
 
 func (r *statusRecorder) WriteHeader(code int) {
