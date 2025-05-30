1b081850ea238a0a5dcad57f781dda25384d7592
Winston Howes <winstonhowes@gmail.com>
2025-05-30 11:05:19 -0700

    Make plugin lookup case-insensitive

--- cmd/integrations/plugins/registry.go
+++ cmd/integrations/plugins/registry.go
@@ -1,15 +1,17 @@
 package plugins
 
+import "strings"
+
 // Builder defines a function that parses CLI args into an Integration.
 type Builder func(args []string) (Integration, error)
 
 var registry = map[string]Builder{}
 
 // Register adds a plugin builder to the registry.
-func Register(name string, b Builder) { registry[name] = b }
+func Register(name string, b Builder) { registry[strings.ToLower(name)] = b }
 
 // Get retrieves a registered builder by name.
-func Get(name string) Builder { return registry[name] }
+func Get(name string) Builder { return registry[strings.ToLower(name)] }
 
 // List returns the registered plugin names.
 func List() []string {
